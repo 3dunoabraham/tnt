44c66293ee30db406de36205e9607fb2743d76c5
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-02-13 07:06:54 -0400

    sidebar fixes

--- src/partials/ims/FilterSidebar.tsx
+++ src/partials/ims/FilterSidebar.tsx
@@ -4,8 +4,12 @@ export default function Component({}) {
     return (<>
     <SidebarHeader />
     <div className='flex-1 w-100'>
-        <div className='pa-2'>
+        <div className='pa-2 w-300px Q_lg_x'>
             Filter
+        </div>
+        
+        <div className='pa-2  Q_xs_lg'>
+            F
         </div>
     </div>
     <div className='flex-1'></div>
--- src/partials/ims/SidebarHeader.tsx
+++ src/partials/ims/SidebarHeader.tsx
@@ -5,8 +5,8 @@ export default function Component({}) {
     return (<>
     <div className='flex px-4'>
         <Link className='tx-white tx-lgx nodeco py-4' href="/">
-            <div className='Q_xs_md'>IMS</div>
-            <div className='Q_md_x'>Inventory</div>
+            <div className='Q_xs_lg'>IMS</div>
+            <div className='Q_lg_x'>Inventory</div>
         </Link>
     </div>
     </>)
